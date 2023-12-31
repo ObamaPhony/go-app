fa96c41583c2b8bed0f6f82bf98526eb29d56286
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-31 01:00:33 +0000

    refactor(speechAnalysis): MAJOR refactor of speech analysis
    When I first wrote this module, I was young, immature, and had amensia.
    
    I've worked on it on and off since, but now I've rewritten this
    particular module to be more 'Go-like', and less cryptic.
    
    Still lots to do. I plan to host on Fly.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- internal/exec/speech-analysis.go
+++ internal/exec/speech-analysis.go
@@ -4,70 +4,68 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-
-	"io"
-	"io/ioutil"
-	"os"
-	"time"
-
 	log "github.com/inconshreveable/log15"
-
 	pipe "gopkg.in/pipe.v2"
+	"strings"
+	"time"
 )
 
-type SAArguments struct {
-	FileOUTPATH string
-	SAScriptLOC string
-	FileOUT     bool
-	SpeechREQ   io.ReadCloser
+const defaultSpeechAnalyseScriptLocation = "/docker/analyse"
+
+type SpeechAnalysisArgs struct {
+	input          string // Inputted speech for script.
+	scriptLocation string // To be populated via `config` module.
+	debugMode      bool   // Output speech analysis to `stdout`. (TODO: See L51)
 }
 
-func returnSpeechAnalysis(a *SAArguments) (result string, err error) {
-	log.Debug("New bytes buffer created!")
-	buffer := new(bytes.Buffer)
-
-	tempdir, err := ioutil.TempDir("", "speechoutput_")
-	if err != nil {
-		return "", err
-	}
-
-	filename := fmt.Sprintf("%s/speechoutput_%s.json",
-		tempdir,
-		time.Now().Format(time.RFC3339))
-
-	file, err := os.Create(filename)
-	defer file.Close()
-	if err != nil {
-		return "", err
-	}
-
-	pi := pipe.Line(
-		pipe.Read(a.SpeechREQ),
-		pipe.Exec(a.SAScriptLOC),
-		pipe.Tee(buffer),
-	)
-
-	err = pipe.Run(pi)
-	if err != nil {
-		return "", err
-	}
-
-	output := &bytes.Buffer{}
-	if err := json.Compact(output, buffer.Bytes()); err != nil {
-		return "", err
-	}
-
-	return output.String(), nil
-
+type AnalyzedSpeech struct {
+	analysis    string        // Returned JSON from Ben's script.
+	processTime time.Duration // Used for metrics on status page.
 }
 
-// SAReturnASYS takes the SAArguments struct, and returns the analysis
-// from the speech analysis program
-func SAReturnASYS(a *SAArguments) (result string, err error) {
-	if a.FileOUT == true {
-		result, err := returnSpeechAnalysis(a)
-		return result, err
+func AnalyzeSpeech(args *SpeechAnalysisArgs) (AnalyzedSpeech, error) {
+	buf := new(bytes.Buffer)
+	var err error = nil
+	var scriptLocation string
+
+	// Check if script path is defined.
+	if len(strings.TrimSpace(args.scriptLocation)) == 0 {
+		// Assign default script path.
+		scriptLocation = defaultSpeechAnalyseScriptLocation
+	} else {
+		// Otherwise trust args.
+		scriptLocation = args.scriptLocation
 	}
 
-	return "", nil
+	// Start timing.
+	start := time.Now()
+
+	// Begin construction of pipe.
+	pipeline := pipe.Line(
+		pipe.Read(strings.NewReader(args.input)),
+		pipe.Exec(scriptLocation),
+		pipe.Tee(buf))
+
+	// Run pipe, assign to declared `err` var
+	err = pipe.Run(pipeline)
+	if err != nil {
+		// Should we let the controller handle errors, and return a 5xx code?
+		log.Error("Could not run speech analysis pipeline.",
+			log.Ctx{"error": err.Error()})
+		return AnalyzedSpeech{}, err
+	}
+
+	if args.debugMode { // TODO: Refactor to remove `debugMode`, use logging level arg in ENV. Output slog-style.
+		fmt.Printf("JSON output from speech analysis request: %s\n", buf.String())
+	}
+
+	analyzedSpeech := new(AnalyzedSpeech)
+	compacted := new(bytes.Buffer)
+	if err := json.Compact(compacted, buf.Bytes()); err != nil {
+		return AnalyzedSpeech{}, err
+	}
+	analyzedSpeech.analysis = compacted.String()
+	analyzedSpeech.processTime = time.Since(start) // TODO: Maybe directly report to metrics, and associate with request ID?
+
+	return *analyzedSpeech, nil
 }
